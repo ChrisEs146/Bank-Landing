f58d639239e2ba63317b368c77e61e8584f4a1c5
ChrisEs146 <chris.es146@gmail.com>
2023-04-24 16:46:04 -0600

    Add: billing information

---
+++ src/Constants/Billing.ts
@@ -0,0 +1,9 @@
+import { apple, google, bill } from "../assets";
+export const BILLING = {
+  title: "Easily control your billing and invoicing",
+  content:
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores dolorum id numquam illo atque temporibus, dolor fuga laboriosam nisi libero",
+  appStore: apple,
+  googlePlay: google,
+  mainImage: bill,
+};
