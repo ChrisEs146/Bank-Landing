3caba3073b7a8790524ba454a899dac55eb0ee79
ChrisEs146 <chris.es146@gmail.com>
2023-04-24 16:46:51 -0600

    Add: Feedback section's title and content

---
+++ src/Constants/Feedback.ts
@@ -0,0 +1,4 @@
+import { people01, people02, people03, coinbase, binance, dropbox, airbnb } from "../assets";
+export const FEEDBACK = {
+  title: "What people are saying about us",
+  content: "Everything you need to accept card payments and grow your business anywhere.",
