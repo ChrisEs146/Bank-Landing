ce9a51262301be2d4c29d43a7e9335523a45f5be
ChrisEs146 <chris.es146@gmail.com>
2023-04-24 16:47:39 -0600

    Add: Footer title, content and logo

---
+++ src/Constants/Footer.ts
@@ -0,0 +1,5 @@
+import { logo, facebook, twitter, instagram, linkedin } from "../assets";
+export const FOOTER = {
+  title: "HooBank",
+  content: "A new way to make easy and secure payments",
+  logo: logo,
