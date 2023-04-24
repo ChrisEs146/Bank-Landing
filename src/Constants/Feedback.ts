eaa1a9a04941d0c0fb703e5e62e5c3e5bcb7ef87
ChrisEs146 <chris.es146@gmail.com>
2023-04-24 16:47:08 -0600

    Add: Feedback reviews

--- src/Constants/Feedback.ts
+++ src/Constants/Feedback.ts
@@ -2,3 +2,26 @@ import { people01, people02, people03, coinbase, binance, dropbox, airbnb } from
 export const FEEDBACK = {
   title: "What people are saying about us",
   content: "Everything you need to accept card payments and grow your business anywhere.",
+  reviews: [
+    {
+      id: "r-1",
+      name: "Herman Jensen",
+      title: "Founder & Leader",
+      content: "Money is only a tool. It will take you wherever you wish, if you plan carefully.",
+      img: people01,
+    },
+    {
+      id: "r-2",
+      name: "Steve Mark",
+      title: "Founder",
+      content: "Gives me peace of mind when travelling abroad.",
+      img: people02,
+    },
+    {
+      id: "r-2",
+      name: "Kenn Garner",
+      title: "Founder",
+      content: "It's a wonderful platform. Simplifies all my online payments and transactions.",
+      img: people03,
+    },
+  ],
