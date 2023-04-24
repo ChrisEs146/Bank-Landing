07db3bebcfa17cccb722d14661dd287376cd9d11
ChrisEs146 <chris.es146@gmail.com>
2023-04-24 16:48:06 -0600

    Add: Footer links

--- src/Constants/Footer.ts
+++ src/Constants/Footer.ts
@@ -3,3 +3,83 @@ export const FOOTER = {
   title: "HooBank",
   content: "A new way to make easy and secure payments",
   logo: logo,
+  links: [
+    {
+      id: "L-1",
+      title: "External Links",
+      internalLinks: [
+        {
+          id: "L-1.1",
+          name: "Content",
+          link: "https://www.hoobank.com/content/",
+        },
+        {
+          id: "L-1.2",
+          name: "How it Works",
+          link: "https://www.hoobank.com/how-it-works/",
+        },
+        {
+          id: "L-1.3",
+          name: "Create",
+          link: "https://www.hoobank.com/create/",
+        },
+        {
+          id: "L-1.4",
+          name: "Explore",
+          link: "https://www.hoobank.com/explore/",
+        },
+        {
+          id: "L-1.5",
+          name: "Terms & Services",
+          link: "https://www.hoobank.com/terms-and-services/",
+        },
+      ],
+    },
+    {
+      id: "L-2",
+      title: "Community",
+      internalLinks: [
+        {
+          id: "L-2.1",
+          name: "Help Center",
+          link: "https://www.hoobank.com/help-center/",
+        },
+        {
+          id: "L-2.2",
+          name: "Partners",
+          link: "https://www.hoobank.com/partners/",
+        },
+        {
+          id: "L-2.3",
+          name: "Suggestions",
+          link: "https://www.hoobank.com/suggestions/",
+        },
+        {
+          id: "L-2.4",
+          name: "Blog",
+          link: "https://www.hoobank.com/blog/",
+        },
+        {
+          id: "L-2.5",
+          name: "Newsletters",
+          link: "https://www.hoobank.com/newsletters/",
+        },
+      ],
+    },
+    {
+      id: "L-3",
+      title: "Partners",
+      internalLinks: [
+        {
+          id: "L-3.1",
+          name: "Our Partner",
+          link: "https://www.hoobank.com/our-partner/",
+        },
+        {
+          id: "L-3.2",
+          name: "Become a Partner",
+          link: "https://www.hoobank.com/become-a-partner/",
+        },
+      ],
+    },
+  ],
