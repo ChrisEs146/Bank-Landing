0cc77b51eccfad2f8318553a82d514bbedf50b22
ChrisEs146 <chris.es146@gmail.com>
2023-04-24 16:46:16 -0600

    Add: Deals section's information

---
+++ src/Constants/Deals.ts
@@ -0,0 +1,7 @@
+import { card } from "../assets";
+export const DEALS = {
+  title: "Find a better card deal in a few easy steps",
+  content:
+    "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident",
+  mainImage: card,
+};
