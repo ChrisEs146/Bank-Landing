9aedc1769d4b1e46547cffef8c6a4f67a5efebad
ChrisEs146 <chris.es146@gmail.com>
2023-05-02 16:58:02 -0600

    Add: Nav component

---
+++ src/Components/Nav/Nav.tsx
@@ -0,0 +1,31 @@
+import { logo, close, menu } from "../../assets";
+import { useState } from "react";
+import NavList from "./NavList";
+import "./Nav.css";
+
+function Nav() {
+  const [isToggle, setIsToggle] = useState<boolean>(false);
+
+  const handleMenuToggle = (): void => setIsToggle(!isToggle);
+  return (
+    <nav className="nav">
+      <div className="nav__container">
+        <img src={logo} alt="hoobank" className="nav__logo" />
+        <img
+          src={isToggle ? close : menu}
+          alt="Menu icon"
+          className="nav__menu"
+          onClick={handleMenuToggle}
+        />
+        <div className={`nav__mobile-menu ${isToggle ? "visible" : "hidden"}`}>
+          <NavList />
+        </div>
+        <div className="nav__desktop-menu">
+          <NavList />
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Nav;
