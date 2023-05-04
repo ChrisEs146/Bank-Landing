ef91712c47856328bf7c340cdbed13b8e39f4a52
ChrisEs146 <chris.es146@gmail.com>
2023-05-04 16:23:58 -0600

    Update: Hero constants

--- src/Constants/Hero.ts
+++ src/Constants/Hero.ts
@@ -1,7 +1,6 @@
 export const HERO = {
-  title: "The Next Generation Payment Method",
   content:
-    "Our team of experts uses a methodology to identify the credit cards most likly to fit your needs. We examine annual percentage rates and.",
+    "Our team of experts uses a methodology to identify the credit cards most likely to fit your needs. We examine annual percentage rates and annual fees.",
   stats: [
     {
       id: "stat1",
