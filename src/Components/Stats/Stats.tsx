fc8d0c9c6e6d552d628a42b1ddcdc9591d4d5590
ChrisEs146 <chris.es146@gmail.com>
2023-05-10 17:49:06 -0600

    Add: Stats component

--- src/Components/Stats/Stats.tsx
+++ src/Components/Stats/Stats.tsx
@@ -1,15 +1,18 @@
 import { HERO } from "../../Constants/Hero";
 import "./Stats.css";
+
 function Stats() {
   return (
-    <ul className="stats">
-      {HERO.stats.map((stat) => (
-        <li key={stat.id}>
-          <span>{stat.value}</span>
-          <span>{stat.text}</span>
-        </li>
-      ))}
-    </ul>
+    <div className="stats">
+      <ul className="stats__list">
+        {HERO.stats.map((stat) => (
+          <li className="stats__item" key={stat.id}>
+            <span>{stat.value}</span>
+            <span>{stat.text}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
