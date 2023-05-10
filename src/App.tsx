7704f26a3dba51903d88df6b8ae8abed247fff68
ChrisEs146 <chris.es146@gmail.com>
2023-05-10 17:43:56 -0600

    Add: Landing page components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import Nav from "./Components/Nav/Nav";
-import "./App.css";
+import Hero from "./Components/Hero/Hero";
+import Stats from "./Components/Stats/Stats";
+import Features from "./Components/Features/Features";
+import Billing from "./Components/Billing/Billing";
+import Deals from "./Components/Deals/Deals";
+import Testimonials from "./Components/Testimonials/Testimonials";
 
 function App() {
   return (
     <div className="app">
       <Nav />
+      <Hero />
+      <Stats />
+      <Features />
+      <Billing />
+      <Deals />
+      <Testimonials />
     </div>
   );
 }
