13c12b0bc8d10d4772aa16150c07a5500a613f17
ChrisEs146 <chris.es146@gmail.com>
2023-05-11 16:51:22 -0600

    Add: Deals component

---
+++ src/Components/Deals/Deals.tsx
@@ -0,0 +1,19 @@
+import { DEALS } from "../../Constants/Deals";
+import StartButton from "../SubComponents/StartButton";
+import "./Deals.css";
+function Deals() {
+  return (
+    <section className="deals" id="deals">
+      <header className="deals__header text-center">
+        <h2 className="dynamic-h2">{DEALS.title}</h2>
+        <p className="dynamic-p">{DEALS.content}</p>
+        <StartButton />
+      </header>
+      <div className="deals__img">
+        <img src={DEALS.mainImage} alt="Analytics illustrations" />
+      </div>
+    </section>
+  );
+}
+
+export default Deals;
