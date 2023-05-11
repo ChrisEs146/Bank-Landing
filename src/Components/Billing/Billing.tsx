98078c36ca170e7b8b1ffa78c1c929486854401c
ChrisEs146 <chris.es146@gmail.com>
2023-05-11 16:51:02 -0600

    Add: billing component

---
+++ src/Components/Billing/Billing.tsx
@@ -0,0 +1,20 @@
+import { BILLING } from "../../Constants/Billing";
+import "./Billing.css";
+function Billing() {
+  return (
+    <section className="billing" id="billing">
+      <div className="billing__img">
+        <img src={BILLING.mainImage} alt="Transactions illustration" />
+      </div>
+      <header className="billing__header text-center">
+        <h2 className="dynamic-h2">{BILLING.title}</h2>
+        <p className="dynamic-p">{BILLING.content}</p>
+        <div className="billing__stores">
+          <img src={BILLING.appStore} alt="Apple store badge" />
+          <img src={BILLING.googlePlay} alt="Google play badge" />
+        </div>
+      </header>
+    </section>
+  );
+}
+export default Billing;
