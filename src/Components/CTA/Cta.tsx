caabc43acbd379a4cc5f580fb0da4ab44eec5e80
ChrisEs146 <chris.es146@gmail.com>
2023-05-13 17:51:38 -0600

    Add: CTA component

---
+++ src/Components/CTA/Cta.tsx
@@ -0,0 +1,17 @@
+import StartButton from "../SubComponents/StartButton";
+import "./Cta.css";
+function Cta() {
+  return (
+    <div aria-label="Call to action card" className="cta">
+      <header className="cta__header text-center">
+        <h2 className="dynamic-h2">Let's try our service now!</h2>
+        <p className="dynamic-p">
+          Everything you need to accept card payments and grow your business anywhere.
+        </p>
+      </header>
+      <StartButton />
+    </div>
+  );
+}
+
+export default Cta;
