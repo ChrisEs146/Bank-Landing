28bb98f4f78ad56e013ee4b5d0f6f96f829e12d7
ChrisEs146 <chris.es146@gmail.com>
2023-05-15 12:15:33 -0600

    Update: cta component

--- src/Components/CTA/Cta.tsx
+++ src/Components/CTA/Cta.tsx
@@ -4,12 +4,12 @@ function Cta() {
   return (
     <div aria-label="Call to action card" className="cta">
       <header className="cta__header text-center">
-        <h2 className="dynamic-h2">Let's try our service now!</h2>
+        <h2 className="cta__title">Let's try our service now!</h2>
         <p className="dynamic-p">
           Everything you need to accept card payments and grow your business anywhere.
         </p>
       </header>
-      <StartButton />
+      <StartButton text="Get Started" />
     </div>
   );
 }
