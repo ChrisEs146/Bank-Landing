d5f1cc5a5147638dd179df3b5ba87da3e5e168cd
ChrisEs146 <chris.es146@gmail.com>
2023-05-15 12:16:09 -0600

    Update: Features component

--- src/Components/Features/Features.tsx
+++ src/Components/Features/Features.tsx
@@ -4,11 +4,11 @@ import StartButton from "../SubComponents/StartButton";
 import "./Features.css";
 function Features() {
   return (
-    <section id="features" className="features margin-center">
+    <section id="features" className="features center-elem">
       <header className="features__header text-center">
         <h2 className="dynamic-h2">{FEATURES.title}</h2>
         <p className="dynamic-p">{FEATURES.content}</p>
-        <StartButton />
+        <StartButton text="Get Started" />
       </header>
       <ul className="features__list">
         {FEATURES.features.map((feature) => (
